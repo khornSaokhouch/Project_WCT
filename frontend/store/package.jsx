74986fd25e5d4568c5d8e5a19c30e6567d1c972b
kheangsenghorng <165351249+kheangsenghorng@users.noreply.github.com>
2025-01-24 16:43:49 +0700

    Update package.jsx

--- frontend/store/package.jsx
+++ frontend/store/package.jsx
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const API_URL = "https://bookingtour-psi.vercel.app/api/tours";
+const API_URL = "https://tour-booking-backend-etao.onrender.com/api/tours";
 
 export const useTourStore = create((set) => ({
   tours: [],
